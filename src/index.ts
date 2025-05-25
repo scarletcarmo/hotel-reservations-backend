c971fc7548f9d0f2cd58eff3e51d6bfec326fb68
Andres9792 <andresxavymg4@gmail.com>
2025-05-25 10:47:27 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('¡Hola desde Express con TypeScript!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
